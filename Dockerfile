a75d3dd639295a446caf90a300dac775eeb78047
Emmanuel Gomez <emmanuel@gomez.io>
2016-01-06 00:36:49 -0800

    Rework with Ubuntu base & Confluent package.

--- Dockerfile
+++ Dockerfile
@@ -1,38 +1,27 @@
-FROM develar/java
+FROM nordstrom/java:8
 
-ENV KAFKA_VERSION=0.8.2.2 \
-    SCALA_VERSION=2.10
-ENV KAFKA_RELEASE ${SCALA_VERSION}-${KAFKA_VERSION}
+ENV KAFKA_VERSION=0.9.0.0 \
+    SCALA_VERSION=2.11.7
 
-RUN apk --update add ca-certificates curl jq gnupg tar bash && \
-    eval $(gpg-agent --daemon) && \
-    mkdir -p /kafka/config /kafka/data /kafka/logs /kafka/templates && \
-    cd /tmp && \
-    MIRROR=`curl -sSL https://www.apache.org/dyn/closer.cgi\?as_json\=1 | jq -r '.preferred'` && \
-    curl -sSLO "${MIRROR}/kafka/${KAFKA_VERSION}/kafka_${KAFKA_RELEASE}.tgz" && \
-    curl -sSLO https://dist.apache.org/repos/dist/release/kafka/${KAFKA_VERSION}/kafka_${KAFKA_RELEASE}.tgz.asc && \
-    curl -sSL  https://kafka.apache.org/KEYS | gpg -q --import - && \
-    gpg -q --verify kafka_${KAFKA_RELEASE}.tgz.asc && \
-    tar -zx -C /kafka --strip-components=1 --no-same-owner -f kafka_${KAFKA_RELEASE}.tgz --wildcards '*/bin/*.sh' '*/libs/' && \
-    cd /kafka/libs && \
-    curl -sSLO http://repo1.maven.org/maven2/org/slf4j/slf4j-log4j12/1.7.9/slf4j-log4j12-1.7.9.jar && \
-    apk del curl jq gnupg tar && \
-    rm -rf /tmp/* /var/cache/apk/* /root/.gnupg
-    #useradd --system -d /kafka --user-group kafka && \
-    #chmod a+rwx /kafka/data /kafka/logs
+COPY confluent_platform_2_0.key /tmp
+RUN echo "deb http://packages.confluent.io/deb/2.0 stable main" > /etc/apt/sources.list.d/confluent.list \
+ && apt-key add /tmp/confluent_platform_2_0.key
+RUN mkdir -p /kafka/config /kafka/data /kafka/logs /kafka/templates \
+ && apt-get update -qy \
+ && apt-get install -qy confluent-kafka-${SCALA_VERSION}=${KAFKA_VERSION}-1 \
+ && rm /etc/kafka/server.properties /etc/kafka/log4j.properties /etc/kafka/tools-log4j.properties
 
 ADD  config /kafka/templates/
 COPY entrypoint.sh /
 
-ENV PATH="/kafka/bin:$PATH" \
-    JMX_PORT=7203 \
+ENV JMX_PORT=7203 \
     KAFKA_JVM_PERFORMANCE_OPTS="-XX:MetaspaceSize=48m -XX:MaxMetaspaceSize=48m -XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:InitiatingHeapOccupancyPercent=35 -XX:+DisableExplicitGC -Djava.awt.headless=true"
 
 #USER kafka
 
 ENTRYPOINT [ "/entrypoint.sh" ]
 
-CMD [ "kafka-server-start.sh", "/kafka/config/server.properties" ]
+CMD [ "kafka-server-start", "/etc/kafka/server.properties" ]
 
 # Kafka, JMX
 EXPOSE 9092 7203
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 # Only allocate a broker id and configure the system the first time this container starts.
 # A Kubernetes volume mount will preserve the config if the container dies and is restarted
 # in the same node.
-if [ ! -f /kafka/config/server.properties ]; then
+if [ ! -f /etc/kafka/server.properties ]; then
 	# Create a ZK connection string for the servers and the root.
 	ZOOKEEPER_CONNECT=()
 	IFS=\, read -a servers <<< "${ZOOKEEPER_SERVERS:=zookeeper:2181}"
@@ -15,14 +15,15 @@ if [ ! -f /kafka/config/server.properties ]; then
 	echo "Using ZK at ${ZOOKEEPER_CONNECT}"
 
 	# Create the ZK root if it doesn't already exist.
-	echo create "$ZOOKEEPER_ROOT" 0 | /kafka/bin/zookeeper-shell.sh $ZOOKEEPER_SERVERS &> /dev/null
+	echo create "$ZOOKEEPER_ROOT" 0 | zookeeper-shell $ZOOKEEPER_SERVERS &> /dev/null
 
 	if [ -z $BROKER_ID ]; then
 		# Create node to use for id allocation.
-		echo create /kafka_id_alloc 0 | /kafka/bin/zookeeper-shell.sh $ZOOKEEPER_CONNECT &> /dev/null
+		# TODO: echo create $ZOOKEEPER_ROOT/id_alloc 0 | /usr/bin/zookeeper-shell $ZOOKEEPER_CONNECT &> /dev/null
+		echo create /kafka_id_alloc 0 | zookeeper-shell $ZOOKEEPER_CONNECT &> /dev/null
 
-		# Allocate an id by writing to a node and retriving its version number.
-		BROKER_ID=`echo set /kafka_id_alloc 0 | /kafka/bin/zookeeper-shell.sh $ZOOKEEPER_CONNECT 2>&1 | grep dataVersion | cut -d' ' -f 3`
+		# Allocate an id by writing to a node and retrieving its version number.
+		BROKER_ID=`echo set /kafka_id_alloc 0 | zookeeper-shell $ZOOKEEPER_CONNECT 2>&1 | grep dataVersion | cut -d' ' -f 3`
 		echo "Allocated broker id ${BROKER_ID}."
 	else
 		echo "Using broker id ${BROKER_ID}."
@@ -30,10 +31,10 @@ if [ ! -f /kafka/config/server.properties ]; then
 
 	# Create the config file.
 	sed -e "s|\${BROKER_ID}|$BROKER_ID|g" \
-			-e "s|\${ADVERTISED_HOST_NAME}|$ADVERTISED_HOST_NAME|g " \
-			-e "s|\${ZOOKEEPER_CONNECT}|$ZOOKEEPER_CONNECT|g" /kafka/templates/server.properties.template > /kafka/config/server.properties
+		-e "s|\${ADVERTISED_HOST_NAME}|$ADVERTISED_HOST_NAME|g " \
+		-e "s|\${ZOOKEEPER_CONNECT}|$ZOOKEEPER_CONNECT|g" /kafka/templates/server.properties.template > /etc/kafka/server.properties
 
-	cp /kafka/templates/log4j.properties /kafka/templates/tools-log4j.properties /kafka/config
+	cp /kafka/templates/log4j.properties /kafka/templates/tools-log4j.properties /etc/kafka
 fi
 
 cd /kafka
